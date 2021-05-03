5d9641e2378db9bcff22fa688eb578a63390f9ab
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-05-03 20:42:30 +0530

    Add files via upload

---
+++ src/main/BatteryCurrentReading.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class BatteryCurrentReading {
+	HashMap<String,Integer> currentValues = new HashMap<String,Integer>();
+	
+	
+
+	public HashMap<String, Integer> checkCurrentRangeAndReadings(List<Integer> readings) {
+		if(readings == null || readings.contains(null) || readings.isEmpty() || readings.size() == 1) {
+			return null;
+		}
+		
+		Collections.sort(readings);
+		int count = 1;
+		int	minRange = readings.get(0),maxRange = readings.get(0);
+		
+		 for (int i = 1; i < readings.size(); ++i) {
+		      if ((readings.get(i) - readings.get(i-1)) < 2) {		        
+		        ++count;
+		        if(minRange > readings.get(i))
+		        	minRange = readings.get(i);
+		        if(maxRange < readings.get(i))
+		        	maxRange = readings.get(i);
+		      }
+		      
+		      else {		   
+		    	currentValues.put(minRange+ "-"+maxRange, count);
+		    	minRange = maxRange = readings.get(i);
+		        count = 1;  
+        
+		      }
+
+		    }
+	    	currentValues.put(minRange+ "-"+maxRange, count);
+		return currentValues;
+	}
+	
+
+}
